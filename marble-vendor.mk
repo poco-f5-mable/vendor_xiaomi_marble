899d7332797e38a35619b81ba95f8a358e7424c4
kenway214 <kenway214@outlook.com>
2023-12-17 20:53:55 +0300

    [SQUASH] marble: Import Oneplus Dolby
    marble: dolby: Allow some apps to open DaxUI as eq
    
    Signed-off-by: kenway214 <kenway214@outlook.com>
    
    marble: Import DaxUI (Dolby UI) from OnePlus
    
    Signed-off-by: kenway214 <kenway214@outlook.com>
    
    marble: Update daxService to a14 from Sony pdx234
    
    From:
    sssi-user-14-67.1.A.2.112-1-release-keys
    
    * Fixed fatal exception
    * Fixed UI glitch
    
    Signed-off-by: kenway214 <kenway214@outlook.com>
    
    marble: DaxUI: Recompile to API 34
    
    * Android 14
    
    Signed-off-by: kenway214 <kenway214@outlook.com>
    
    marble: Hide Dolby Atmos from Launcher
    
    Moved to settings>audio>dolby atmos
    XiaomiParts commit required
    
    Signed-off-by: kenway214 <kenway214@outlook.com>
    Co-authored-by: johnmart19 <johnivan19999@gmail.com>
    Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Co-authored-by: Murat Kozan <muratkozan350@gmail.com>

--- marble-vendor.mk
+++ marble-vendor.mk
@@ -1222,7 +1222,16 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/marble/proprietary/vendor/lib64/vendor.xiaomi.hardware.campostproc@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.xiaomi.hardware.campostproc@1.0.so \
     vendor/xiaomi/marble/proprietary/vendor/lib64/vendor.xiaomi.hardware.quickcamera@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.xiaomi.hardware.quickcamera@1.0.so
 
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/marble/proprietary/system_ext/etc/sysconfig/config-com.dolby.daxappui.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/config-com.dolby.daxappui.xml \
+    vendor/xiaomi/marble/proprietary/system_ext/etc/sysconfig/config-com.dolby.daxservice.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/config-com.dolby.daxservice.xml \
+    vendor/xiaomi/marble/proprietary/system_ext/etc/sysconfig/hiddenapi-com.dolby.daxservice.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/hiddenapi-com.dolby.daxservice.xml \
+    vendor/xiaomi/marble/proprietary/system/lib/vendor.dolby.hardware.dms@2.0.so:$(TARGET_COPY_OUT_SYSTEM)/lib/vendor.dolby.hardware.dms@2.0.so \
+    vendor/xiaomi/marble/proprietary/system/lib64/vendor.dolby.hardware.dms@2.0.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/vendor.dolby.hardware.dms@2.0.so \
+
 PRODUCT_PACKAGES += \
+    DaxUI \
+    daxService \
     libagm \
     libar-pal \
     libssccalapi@2.0 \
